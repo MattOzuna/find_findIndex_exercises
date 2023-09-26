c5de82459a2cd87b8e2e89f312bec801c2041e7b
Matt Ozuna <m.ozuna22@gmail.com>
2023-09-25 21:46:56 -0400

    finished exercises

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,11 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(obj){
+    for (let key in obj) return (obj[key] === username);
+  })
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +31,22 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  return usersArray.find(function(obj){
+    for (i = 0; i < usersArray.length; i++){
+    if (obj.username === username){
+      usersArray.splice(i, 1)
+      return true;
+    }
+  }
+})
+ //another option
+  // return usersArray.find(function(obj){
+  //   let i = usersArray.indexOf(obj);
+  //   for (let key in obj)
+  //   if (obj[key] === username){
+  //     usersArray.splice(i, 1)
+  //     return true;
+  //   }
+  // })
+}
